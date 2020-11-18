4433924b4d1796cb988d7bd588ad03abc596a255
Zhenyu Zheng <zheng.zhenyu@outlook.com>
2020-11-18 16:58:47 +0800

    Update terasort-appcds.sh

--- terasort-appcds.sh
+++ terasort-appcds.sh
@@ -13,3 +13,5 @@ HDFS_SECONDARYNAMENODE_USER=root
 YARN_RESOURCEMANAGER_USER=root
 HADOOP_SECURE_DN_USER=yarn
 YARN_NODEMANAGER_USER=root
+
+bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.0.jar teragen -Dmapred.map.tasks=100 -Dmapreduce.map.java.opts="-Xshare:off -XX:+UseAppCDS -XX:DumpLoadedClassList=/opt/terasort.lst" ${rows} terasort/tera1-input
